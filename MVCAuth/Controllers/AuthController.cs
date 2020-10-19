7e4a88702b64db1b6c4383e019e8a74d50f8489b
Info@proemcs.com <info@proemcs.com>
2020-10-19 12:19:27 +0200

    Add project files.

---
+++ MVCAuth/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MVCAuth.Models;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
+
+namespace MVCAuth.Controllers
+{
+    public class AuthController : Controller
+    {
+        private IConfiguration _config;
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login([FromForm] LoginModel user)
+        {
+            if (user.username == "emad" && user.password == "emad")
+            {
+                var token = GenerateToken(user, 100);
+                Response.Cookies.Append("token", token);
+                return Ok(token);
+            }
+            return Unauthorized();
+        }
+        private string GenerateToken(LoginModel Authuser, int timeExpiration)
+        {
+            //Header==================
+            var SecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:secretkey"]));
+            var credential = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
+            //Payload=================
+            var Claims = new[]
+            {
+                new Claim("Id",Authuser.username+""),
+            };
+            #region for Role Identity
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(Claims, "Token");
+            // Adding roles code
+            // Roles property is string collection but you can modify Select code if it it's not
+            claimsIdentity.AddClaims(new List<Claim> { new Claim(ClaimTypes.Role, "admin") });
+            #endregion
+
+            var Token = new JwtSecurityToken(
+                issuer: "beldor@beldor.com",
+                audience: "beldor@beldor.com",
+                claimsIdentity.Claims,
+                expires: DateTime.Now.AddDays(timeExpiration),
+                signingCredentials: credential
+                );
+
+            var encodeToken = new JwtSecurityTokenHandler().WriteToken(Token);
+            return encodeToken;
+        }
+    }
+}
---
+++ MVCAuth/Startup.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MVCAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["Jwt:secretkey"])),
+                    ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                };
+                cfg.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["token"];
+                        return Task.CompletedTask;
+                    },
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
